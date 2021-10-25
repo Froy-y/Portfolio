0fe5d91d2a8b7d9b5a351f88cc2640c47e6a0b7c
Froylan <froy.push@gmail.com>
2021-10-24 23:29:06 -0500

    trying form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -78,7 +78,7 @@ export default function Contact() {
           </div>
           <button
             type="submit"
-            className="text-white bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
+            className="text-black bg-gray-800 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg">
             Submit
           </button>
         </form>
