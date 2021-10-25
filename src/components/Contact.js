c4e844081d9cefa88ea580094f25900a22ca1bc5
Froylan <froy.push@gmail.com>
2021-10-24 23:24:33 -0500

    trying form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -28,8 +28,9 @@ export default function Contact() {
     <section id="contact" className="relative">
       <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
-          netlify
+          data-netlify="true"
           name="contact"
+          method="post"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0">
           <h2 className="text-black sm:text-4xl text-3xl mb-1 font-medium title-font">
