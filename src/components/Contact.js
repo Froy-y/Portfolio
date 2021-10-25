326feb7f5080aea68818995d18f223e5f37b9ce9
Froylan <froy.push@gmail.com>
2021-10-24 23:39:40 -0500

    trying form2

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -26,7 +26,7 @@ export default function Contact() {
 
   return (
     <section id="contact" className="relative">
-      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap" netlify-recaptcha>
+      <div className="container px-5 py-10 mx-auto flex sm:flex-nowrap flex-wrap">
         <form
           data-netlify="true"
           name="contactInformation"
