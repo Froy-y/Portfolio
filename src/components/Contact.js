39ab56ef7b800425b84a3305f91cbbbcf2f959b0
Froylan <froy.push@gmail.com>
2021-10-25 00:24:42 -0500

    form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -18,7 +18,7 @@ export default function Contact() {
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
-      body: encode({ "form-name": "contact", name, email, message }),
+      body: encode({ "form-name": "test", name, email, message }),
     })
       .then(() => alert("Message sent!"))
       .catch((error) => alert(error));
@@ -34,7 +34,7 @@ export default function Contact() {
           name="test"
           onSubmit={handleSubmit}
           className="lg:w-1/3 md:w-1/2 flex flex-col md:mx-auto w-full md:py-8 mt-8 md:mt-0">
-          <input type="hidden" name="test" value="test" />  
+          <input type="hidden" name="form-name" value="test" />  
           <h2 className="text-black sm:text-4xl text-3xl mb-1 font-medium title-font">
             Hire Me
           </h2>
